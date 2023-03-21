23da3b3572f49bd098a64a40bfb30fba97b3c9fc
Jussi Lemmetyinen <jussi.lemmetyinen@fintraffic.fi>
2023-03-21 09:10:28 +0200

    Add dependencies

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,13 +1,24 @@
-import { web } from "projen";
+import { web, javascript } from "projen";
 const project = new web.NextJsTypeScriptProject({
   defaultReleaseBranch: "main",
   name: "jussi-resume-v2",
   projenrcTs: true,
-
-  // deps: [],                /* Runtime dependencies of this module. */
+  prettier: true,
+  prettierOptions: {
+    settings: {
+      bracketSpacing: true,
+      printWidth: 120,
+      singleQuote: true,
+      tabWidth: 2,
+      trailingComma: javascript.TrailingComma.ALL,
+      useTabs: false,
+      arrowParens: javascript.ArrowParens.AVOID,
+    }
+  },
+  deps: ["autoprefixer@10.4.14", "classnames@2.3.2", "ts-pattern@^4.0.3", "@headlessui/react@1.7.5, @heroicons/react@^1.0.6"],
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
+  devDeps: ["cssnano@5.1.15", "@tailwindcss/forms@0.5.3", "@tailwindcss/typography@0.5.9"],
   // packageName: undefined,  /* The "name" in package.json. */
-  // tailwind: true,          /* Setup Tailwind CSS as a PostCSS plugin. */
+  tailwind: true,
 });
 project.synth();
