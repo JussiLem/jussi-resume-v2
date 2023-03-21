459da83ff4aeee1b4fac4c4db7dd5a99ef38853a
Jussi Lemmetyinen <jussi.lemmetyinen@fintraffic.fi>
2023-03-21 12:48:06 +0200

    Initial stuff

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,5 @@
-import { web, javascript } from "projen";
+import {javascript, web} from "projen";
+
 const project = new web.NextJsTypeScriptProject({
   defaultReleaseBranch: "main",
   name: "jussi-resume-v2",
@@ -15,10 +16,35 @@ const project = new web.NextJsTypeScriptProject({
       arrowParens: javascript.ArrowParens.AVOID,
     }
   },
-  deps: ["autoprefixer@10.4.14", "classnames@2.3.2", "ts-pattern@^4.0.3", "@headlessui/react@1.7.5, @heroicons/react@^1.0.6"],
+  tsconfig: {
+    compilerOptions: {
+      rootDir: 'src',
+//      rootDir: undefined,
+//      module: 'esnext',
+//      target: 'esnext',
+//      esModuleInterop: true,
+    },
+    include: [
+      "pages/**/*.tsx",
+      "src/**/*.ts",
+    ],
+  },
+  tsconfigDev: {
+    compilerOptions: {
+//      module: 'esnext',
+      target: 'esnext',
+//      esModuleInterop: true,
+    },
+    include: [
+      "pages/**/*",
+      "src/**/*.ts",
+    ],
+  },
+  packageManager: javascript.NodePackageManager.NPM,
+  deps: ["autoprefixer@10.4.14", "classnames@2.3.2", "ts-pattern@^4.0.3", "@headlessui/react@1.7.13", "@heroicons/react@^1.0.6"],
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  devDeps: ["cssnano@5.1.15", "@tailwindcss/forms@0.5.3", "@tailwindcss/typography@0.5.9"],
+  devDeps: ["@tailwindcss/forms@0.5.3", "@tailwindcss/typography@0.5.9", "postcss@8.4.21", "postcss-preset-env@8.0.1", "sass@1.59.3"],
   // packageName: undefined,  /* The "name" in package.json. */
-  tailwind: true,
+  tailwind: false,
 });
 project.synth();
---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import 'tailwindcss/tailwind.css';
+import '../src/globalStyles.scss';
+
+import type {AppProps} from 'next/app';
+import {memo} from 'react';
+
+const MyApp = memo(
+  ({ Component, pageProps }: AppProps): JSX.Element => (
+    <>
+      <Component {...pageProps} />
+    </>
+  ),
+);
+
+export default MyApp;
