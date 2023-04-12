ff31ddff09b8260d68a1d5c9d22b5bac89a7900f
Jussi Lemmetyinen <jussi.lemmetyinen@gofore.com>
2023-04-12 09:59:07 +0300

    Add bucket for nextjs

---
+++ infra/src/cloudfront-helper.ts
@@ -0,0 +1,38 @@
+import { aws_cloudfront as cloudfront } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+const defaultCloudfrontFunction = (scope: Construct): cloudfront.Function => {
+  // generate a stable unique id for the cloudfront function and use it
+  // both for the function name and the logical id of the function so if
+  // it is changed the function will be recreated.
+  // see https://github.com/aws/aws-cdk/issues/15523
+  const functionId = `SetHttpSecurityHeaders${scope.node.addr}`;
+
+  return new cloudfront.Function(scope, 'SetHttpSecurityHeaders', {
+    functionName: functionId,
+    code: cloudfront.FunctionCode.fromInline(
+      "function handler(event) { var response = event.response; \
+      var headers = response.headers; \
+      headers['strict-transport-security'] = { value: 'max-age=63072000; includeSubdomains; preload'}; \
+      headers['content-security-policy'] = { value: \"default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'\"}; \
+      headers['x-content-type-options'] = { value: 'nosniff'}; \
+      headers['x-frame-options'] = {value: 'DENY'}; \
+      headers['x-xss-protection'] = {value: '1; mode=block'}; \
+      return response; \
+    }",
+    ),
+  });
+};
+
+const getCloudfrontFunction = (httpSecurityHeaders: boolean, scope: Construct) =>
+  httpSecurityHeaders ? defaultCloudfrontFunction(scope) : undefined;
+
+export const CloudFrontDistributionForS3 = (
+  scope: Construct,
+  // sourceBucket: s3.IBucket,
+  // cloudFrontDistributionProps?: cloudfront.DistributionProps | any,
+  httpSecurityHeaders: boolean = true,
+  // originPath?: string,
+) => {
+  getCloudfrontFunction(httpSecurityHeaders, scope);
+};
--- infra/src/resume/resume.ts
+++ infra/src/resume/resume.ts
@@ -1,5 +1,23 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { aws_cloudfront as cloudfront, aws_s3 as s3, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { buildS3Bucket } from '../s3-bucket-helper';
+
+export class NextJsServerless extends Construct {
+  // public readonly cloudFrontWebDistribution: cloudfront.Distribution;
+  public readonly cloudFrontFunction?: cloudfront.Function;
+  // public readonly s3BucketInterface: s3.IBucket;
+  public readonly s3Bucket?: s3.Bucket;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+    this.s3Bucket = buildS3Bucket(this, {
+      bucketProps: {
+        autoDeleteObjects: true,
+        removalPolicy: RemovalPolicy.DESTROY,
+      },
+    });
+  }
+}
 
 type ResumeProps = {
   nextStaticDir: string;
@@ -7,7 +25,7 @@ type ResumeProps = {
 export class Resume extends Stack {
   constructor(scope: Construct, id: string, props: ResumeProps) {
     super(scope, id, props);
-
+    new NextJsServerless(this, 'NextJs');
     // cm.Certificate.fromCertificateArn(this, 'id', 'certArn');
   }
 }
