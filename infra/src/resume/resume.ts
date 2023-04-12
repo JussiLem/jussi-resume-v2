2994b1d08609c65f3c0fa348cfb391b66092109f
Jussi Lemmetyinen <jussi.lemmetyinen@gofore.com>
2023-04-12 16:17:08 +0300

    Fix asset path

--- infra/src/resume/resume.ts
+++ infra/src/resume/resume.ts
@@ -1,7 +1,5 @@
-import path from 'path';
 import {
   aws_cloudfront as cloudfront,
-  aws_lambda_nodejs as nodejs,
   aws_s3 as s3,
   aws_s3_deployment as deploy,
   Stack,
@@ -23,14 +21,14 @@ export class NextJsServerless extends Construct {
     NagSuppressions.addResourceSuppressions(this.s3Bucket, [{ id: 'AwsSolutions-S1', reason: 'For money saving' }]);
 
     new deploy.BucketDeployment(this, 'NoCacheFilesDeployment', {
-      sources: [deploy.Source.asset('../../../.next/server')],
+      sources: [deploy.Source.asset('../.next/server')],
       destinationBucket: this.s3Bucket,
-      cacheControl: [deploy.CacheControl.fromString('public, max-age=31536000, immutable')],
+      cacheControl: [deploy.CacheControl.fromString('public, max-age=0, must-revalidate')],
       prune: false,
     });
 
     new deploy.BucketDeployment(this, 'StaticFilesDeployment', {
-      sources: [deploy.Source.asset('../../../.next/static')],
+      sources: [deploy.Source.asset('../.next/static')],
       destinationBucket: this.s3Bucket,
       cacheControl: [deploy.CacheControl.fromString('public, max-age=31536000, immutable')],
       prune: false,
@@ -45,10 +43,10 @@ export class Resume extends Stack {
   constructor(scope: Construct, id: string, props: ResumeProps) {
     super(scope, id, props);
     new NextJsServerless(this, 'NextJs');
-    new nodejs.NodejsFunction(this, 'RegenerationLambda', {
-      handler: 'index.handler',
-      entry: path.join('.next', 'regeneration-lambda'),
-    });
+    // new nodejs.NodejsFunction(this, 'RegenerationLambda', {
+    //   handler: 'index.handler',
+    //   entry: path.join('.next', 'regeneration-lambda'),
+    // });
     // cm.Certificate.fromCertificateArn(this, 'id', 'certArn');
   }
 }
